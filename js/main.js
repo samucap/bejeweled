4643253dd8c0e11877941e90a4143869e33234c6
Samuel Park <samuel.d.g.park@gmail.com>
2015-08-24 23:44:16 -0700

    board readjust, using objects

--- js/main.js
+++ js/main.js
@@ -1,25 +1,78 @@
-var container = document.getElementById('container');
-
-function board(){
-	for (var x = 0; x < 8; x++){
-		var div = document.createElement("div");
-		container.appendChild(div);
-		div.className = "row";
-
-			for (var i = 0; i < 8; i++){
-				var cells = document.createElement("div");
-				var rows = document.getElementsByClassName("row");
-				var y = rows[x];
-				y.appendChild(cells);
-				cells.style.width = "60px";
-				cells.style.height = "60px";
-				cells.style.border = "1px blue solid";
-				cells.style.display = "inline-block";
-				cells.style.margin = "4px";
-				y.style.textAlign = "center";
-			}
+function Grid(width, height, container){
+	this.width = width;
+	this.height = height;
+	this.cells = [];
+	this.containerEl = document.getElementById(container);
+	
+	this.populate = function(){
+		for (var i = 0; i < this.width * this.height; i++){
+			var jewel = whichJewel();
+			this.cells.push(jewel);
+		}
 	}
+	
+	this.move = function(e){
+		var clickedElement = e.currentTarget;
+		var index = parseInt( clickedElement.getAttribute('data-index') , 10 );
+		var clickedJewel = this.cells[index];
+		console.log( clickedJewel )
+	}
+
+
+	this.render = function(){
+		for (var i = 0; i < this.width * this.height ; i++){
+			var currentJewel = this.cells[i];
+			var isNewLine = (i !== 0 && i % 8 === 0);
+			var div = document.createElement('div');
+			div.addEventListener('click', this.move.bind(this) );
+			div.innerHTML = currentJewel.type;
+			if (isNewLine){
+				var div2 = document.createElement('div');
+				this.containerEl.appendChild(div2);
+			}
+
+			div.className = "cell";
+			div.setAttribute('data-index', i);
+			this.containerEl.appendChild(div);
+		}
+	}
+
+
+}
+
+function Jewel (type){
+	this.type = type;	
+}
+
+function whichJewel(){
+	var jewel;
+	var random = Math.random();
+	if (random < 0.2){
+		jewel = new Jewel('blue');
+	}
+
+	else if ((random > 0.2) && (random < 0.4)){
+		jewel = new Jewel('red');
+	}
+
+	else if ((random > 0.4) && (random < 0.6)){
+		jewel = new Jewel('yellow');
+	}
+
+	else if ((random > 0.6) && (random < 0.8)){
+		jewel = new Jewel('green');
+	}
+
+	else {
+		jewel = new Jewel('white');
+	}
+	return jewel;
 };
 
-board();
 
+var board = new Grid(8, 8, 'container');
+
+
+
+board.populate();
+board.render();
