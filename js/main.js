b6fc5a7c38b8fe9d58669232d270ad368404bbe5
Samuel Park <samuel.d.g.park@gmail.com>
2015-08-25 18:24:27 -0700

    swap tiles function added

--- js/main.js
+++ js/main.js
@@ -3,113 +3,117 @@
 function Grid(width, height, container){
 	this.width = width;
 	this.height = height;
-	this.cells = [];
-	this.containerEl = document.getElementById(container);
+	this.columns = [];
 	
 //Creates my cells, and it populates it with jewels
 	this.populate = function(){
-		for (var i = 0; i < this.width * this.height; i++){
-			var jewel = whichJewel();
-			this.cells.push(jewel);
-		}
-	}
-
-//Handler for click event on my cells
-	this.move = function(e){
-		var clickedElement = e.currentTarget;
-		var index = parseInt( clickedElement.getAttribute('data-index') , 10 );
-		var clickedJewel = this.cells[index];
-		var firstSquare = getFirstSquare(this);
-
-		if(firstSquare){
-			// logic for second click
-			var b = clickedJewel;
-			console.log(b);
-			firstSquare.selected = false;
-		} else {
-			// logic for first click
-			clickedJewel.selected = true;
-			var a = clickedJewel;
-			console.log(a);
-			// function(index){
-			// 	console.log(index);
-			// }
+		for (var i = 0; i < this.width; i++){
+			this.columns[i] = [];
+			for (var x = 0; x < this.height; x++){
+				var jewel = whichJewel(i, x);
+				this.columns[i].push(jewel);
+			}
 		}
 	}
 
 //Renders my grid on dom, while setting an event listener
 //to all my cells
 	this.render = function(){
-		for (var i = 0; i < this.width * this.height; i++){
-			var currentJewel = this.cells[i];
-			var isNewLine = (i !== 0 && i % 8 === 0);
+		document.body.removeChild(document.getElementById('container'));
+		var container = document.createElement('div');
+		container.id = 'container';
+		document.body.appendChild(container);
+		for (var i = 0; i < this.width; i++){
 			var div = document.createElement('div');
-			div.addEventListener('click', this.move.bind(this) );
-			div.innerHTML = currentJewel.type;
-			if (isNewLine){
-				var div2 = document.createElement('div');
-				this.containerEl.appendChild(div2);
+			div.className = 'column';
+			container.appendChild(div);
+			for (var x = 0; x < this.height; x++){
+				var currentJewel = this.columns[i][x];
+				var tile = document.createElement('div');
+				tile.className = "cell";
+				tile.addEventListener('click', this.move.bind(this) );
+				tile.setAttribute('data-column', i);
+				tile.setAttribute('data-cell', x)
+				tile.innerHTML = currentJewel.type;
+				div.appendChild(tile);
 			}
-
-			div.className = "cell";
-			div.setAttribute('data-index', i);
-			this.containerEl.appendChild(div);
 		}
 	}
 
+//Handler for click event on my cells
+	this.move = function(e){
+		var clickedElement = e.currentTarget;	
+		var columnIndex = parseInt( clickedElement.getAttribute('data-column') , 10 );
+		var cellIndex = parseInt( clickedElement.getAttribute('data-cell') , 10 );
+		var clickedJewel = this.columns[columnIndex][cellIndex];
+		var firstSquare = getFirstSquare(this);
+		console.log(firstSquare);
+		if(firstSquare){
+			// logic for second click
+			var secondSquare = clickedJewel;
+			console.log(secondSquare);
+			var fsType = firstSquare.type;
+			firstSquare.type = secondSquare.type;
+			secondSquare.type = fsType;
+			firstSquare.selected = false;
+			console.log(firstSquare.type);
+			console.log(secondSquare.type);
+			this.render();
 
-	// this.toLeft = function(){
-	// 	this.index -= 1;
-
-	// };
-	// this.onTop = function(){
-	// 	index -= 8;
-
-	// };
-	// this.under = function(){
-	// 	index += 8;
-	// };
+		} else {
+			// logic for first click
+			clickedJewel.selected = true;
+			console.log(clickedJewel);
+			// function(index){
+			// 	console.log(index);
+			// }
+		}
+	}
 }
+
 
 //Constructor for my jewels
 
-function Jewel (type){
+function Jewel (type, column, cell){
 	this.type = type;
 	this.selected = false;
-
+	this.column = column;
+	this.cell = cell;
 }
 
 //randomizer for my different jewels
-function whichJewel(){
+function whichJewel(col, cell){
 	var jewel;
 	var random = Math.random();
 	if (random < 0.2){
-		jewel = new Jewel('blue');
+		jewel = new Jewel('blue', col, cell);
 	}
 
 	else if ((random > 0.2) && (random < 0.4)){
-		jewel = new Jewel('red');
+		jewel = new Jewel('red', col, cell);
 	}
 
 	else if ((random > 0.4) && (random < 0.6)){
-		jewel = new Jewel('yellow');
+		jewel = new Jewel('yellow', col, cell);
 	}
 
 	else if ((random > 0.6) && (random < 0.8)){
-		jewel = new Jewel('green');
+		jewel = new Jewel('green', col, cell);
 	}
 
 	else {
-		jewel = new Jewel('white');
+		jewel = new Jewel('white', col, cell);
 	}
 	return jewel;
 };
 
 function getFirstSquare(board){
 	var selectedSquare = null;
-	for (var i = 0; i < board.cells.length; i++) {
-		if (board.cells[i].selected == true) {
-			selectedSquare = board.cells[i];
+	for (var i = 0; i < board.columns.length; i++) {
+		for (var x = 0; x < board.columns[i].length; x++){
+			if (board.columns[i][x].selected == true) {
+				selectedSquare = board.columns[i][x];
+			}
 		}
 	}
 	return selectedSquare;
