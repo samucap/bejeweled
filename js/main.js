5429fb172d3e193f609fd0fc1d5e9523afe86e7f
Samuel Park <samuel.d.g.park@gmail.com>
2015-11-05 01:51:34 -0800

    refactor to where board is now set

--- js/main.js
+++ js/main.js
@@ -16,33 +16,40 @@ function Grid(width, height, container){
 				this.columns[i].push(jewel);
 			}
 		}
+		board.render();
 		this.checkBoard();
 	}
 
 //Renders my grid on dom, while setting an event listener
 //to all my cells
 	this.render = function(){
-		document.body.removeChild(document.getElementById('container'));
+		// document.body.removeChild(document.getElementById('container'));
 		var container = document.createElement('div');
 		container.id = 'container';
 		document.body.appendChild(container);
 		for (var i = 0; i < this.width; i++){
 			var div = document.createElement('div');
+			div.id = 'columnId-' + i;
 			div.className = 'column';
 			container.appendChild(div);
 			for (var j = 0; j < this.height; j++){
 				var currentJewel = this.columns[i][j];
-				var tile = document.createElement('div');
+				var tile = document.createElement('CANVAS');
 				tile.className = "cell";
+				tile.id = i + ", " + j;
 				tile.addEventListener('click', this.move.bind(this) );
+				tile.setAttribute('width', 70);
+				tile.setAttribute('height', 70);
 				tile.setAttribute('data-column', i);
-				tile.setAttribute('data-cell', j)
-				tile.innerHTML = i + ", " + j;
-				tile.style.backgroundColor = currentJewel.type;
+				tile.setAttribute('data-cell', j);
+				var ctx = tile.getContext('2d');
+				ctx.fillStyle = currentJewel.type;
+				ctx.fillRect(0, 0, tile.width, tile.height);
+				tile.style.backgroundColor = 'gray';
 				div.appendChild(tile);
 			}
 		}
-		this.checkBoard();
+		// this.checkBoard();
 	}
 
 	this.checkBoard = function() {
@@ -50,23 +57,171 @@ function Grid(width, height, container){
 			for (var j = 0; j < this.columns[i].length; j++){
 				this.checkForRight( i, j );
 				this.checkForDown( i, j );
-				if ( this.columns[i][j].flaggedForRemoval ){
-					console.log("Being removed: ", this.columns[i][j], "i: ", i, "j: ", j);
-					this.removeJewels(i, j);
+			}
+		}
+		for (var i = 0; i < this.columns.length; i++){
+			for (var j = 0; j < this.columns[i].length; j++){
+				if(this.columns[i][j].flaggedForRemoval === true){
+					return this.removeJewels();
+				}
+			}
+		}
+		console.log('THIS.COLUMNS', this.columns);
+	}
+
+	this.removeJewels = function(){
+		var columnCount = [],
+			cellCount = [];
+		for (var i = 0; i < this.columns.length; i++){
+			columnCount.push({i: i});
+			var currColumn = columnCount[i];
+			currColumn.cellArray = [];
+			for (var j = 0; j < this.columns[i].length; j++){
+				if(this.columns[i][j].flaggedForRemoval === true){
+					this.columns[i].splice(j, 1);
+					var newJewel = whichJewel();
+					this.columns[i].unshift(newJewel);
+					columnCount[i].cellArray.push(j);
+					// columnCount.push(j);
+					// var currCanvas = document.getElementById(i + ", " + j);
+					// var ctx = currCanvas.getContext('2d');
+					// ctx.clearRect(0, 0, currCanvas.width, currCanvas.height);
+					// ctx.fillStyle = newJewel.type;
+					// ctx.fillRect(0, 0, currCanvas.width, currCanvas.height);	
+				}
+			}
+		}
+		// console.log('cellCount:', cellCount);
+		// columnCount.numberOfOccurrences = function(num){
+		// 	var count = 0;
+		// 	for(var i = 0; i < this.length; i++){
+		// 		if(this[i] === num){
+		// 			count += 1;
+		// 		}
+		// 	}
+		// 	return count;
+		// }
+
+		// for(var i = 0; i < columnCount.length; i++){
+		// 	console.log("LOOOKK: ", columnCount.numberOfOccurrences(columnCount[i]));
+		// }
+
+		this.recolor(columnCount);
+	}
+
+	this.recolor = function(columnCount){
+
+		for(var i = 0; i < columnCount.length; i++){
+			var cellArr = columnCount[i].cellArray;
+			if(cellArr.length > 0){
+				// console.log('columnCount[i], ', columnCount[i]);
+				var colIndex = columnCount[i].i;
+				var cellIndex = cellArr[cellArr.length - 1];
+
+				while(cellIndex >= 0){
+					var currCanvas = document.getElementById(colIndex + ", " + cellIndex);
+					var ctx = currCanvas.getContext('2d');
+					ctx.clearRect(0, 0, currCanvas.width, currCanvas.height);
+					ctx.fillStyle = this.columns[colIndex][cellIndex].type;
+					ctx.fillRect(0, 25, currCanvas.width, currCanvas.height);
+					// console.log("CURRACANVA: ", currCanvas);
+					cellIndex--;
 				}
 			}
 		}
 
+		this.checkBoard();
+
+		// columnCount.numberOfOccurrences = function(num){
+		// 	var count = 0;
+		// 	for(var i = 0; i < this.length; i++){
+		// 		if(this[i] === num){
+		// 			count += 1;
+		// 		}
+		// 	}
+		// 	return count;
+		// }
+
+		// columnCount.unique = function(){
+		// 	var whichColumn = [];
+
+		// 	for(var i = 0; i < this.length; i++){
+		// 		if(whichColumn.indexOf(this[i]) === - 1){
+		// 			whichColumn.push(this[i]);
+		// 		}
+		// 	}
+		// 	return whichColumn;
+		// }
+
+		// console.log("uniqueArrColumns: ", columnCount);
+
+		// Count.unique = function(){
+		// 	var startIndex = [];
+
+		// 	for(var i = 0; i < this.length; i++){
+		// 		if(startIndex.indexOf(this[i]) === - 1){
+		// 			startIndex.push(this[i]);
+		// 		}
+		// 	}
+		// 	return startIndex;
+		// }
+
+		// columnCount.organize = function(){
+		// 	var organized = columnCount.sort();
+		// 	console.log("organized", organized);
+		// 	for(var i = 0; i < organized.length; i++){
+
+		// 	}
+		// }
+
+		// console.log("UNIQUEARRCELL: ", cellCount);
+
+		// var columnIndex = columnCount.unique();
+
+		// var cellArray = [];
+
+		// for(var i = 0; i < columnCount.length; i++){
+
+		// 	console.log("LOOOKK: ", columnCount.numberOfOccurrences(columnCount[i]));
+
+		// }
+
+
+		// var currCanvas;
+
+		// for(var i = 0; i < columnIndex.length; i++){
+		// 	var occurrences = columnCount.numberOfOccurrences(columnIndex[i]);
+
+		// 	for(var j = 0; j < occurrences; j++){
+							
+		// 	}
+
+		// }
+
+		// var canvasArr = document.querySelectorAll('canvas');
+		// console.log("canvasArr", canvasArr);
+		// var colors = [];
+
+		// for (var i = 0; i < this.columns.length; i++){
+		// 	for (var j = 0; j < this.columns[i].length; j++){
+		// 		colors.push(this.columns[i][j].type);
+		// 	}
+		// }
+
+		// var count = 0;
+
+		// for (var i = 0; i < canvasArr.length; i++){
+		// 	var currCanvas = canvasArr[i];
+		// 	var ctx = currCanvas.getContext('2d');
+		// 	ctx.clearRect(0, 0, currCanvas.width, currCanvas.height);
+		// 	ctx.fillStyle = colors[count];
+		// 	ctx.fillRect(0, 0, currCanvas.width, currCanvas.height);
+		// 	count++	
+		// }
+
+
 	}
 
-	this.removeJewels = function(i, j){
-
-		this.columns[i].splice(j, 1);
-		var newJewel = whichJewel();
-		console.log("column being added to: ", i)
-		this.columns[i].unshift(newJewel);
-		board.render();
-	}
 
 	//CHECK RIGHT
 	this.checkForRight = function(columnIndex, rowIndex){
@@ -135,7 +290,6 @@ function Grid(width, height, container){
 				= this.columns[ this.selectedJewelPos[0] ][ this.selectedJewelPos[1] ];
 				this.columns[ this.selectedJewelPos[0] ][ this.selectedJewelPos[1] ] = tempJewel;
 				
-				this.checkBoard();
 
 				if( this.columns[ this.selectedJewelPos[0] ][ this.selectedJewelPos[1] ].flaggedForRemoval === false && this.columns[ secondClickedPos[0] ][ secondClickedPos[1] ].flaggedForRemoval === false ){
 				
@@ -143,7 +297,7 @@ function Grid(width, height, container){
 					= this.columns[ secondClickedPos[0] ][ secondClickedPos[1] ];
 					this.columns[ secondClickedPos[0] ][ secondClickedPos[1] ] = tempJewel;
 
-					board.render();
+					// board.render();
 				}
 
 				else if ( this.columns[ this.selectedJewelPos[0] ][ this.selectedJewelPos[1] ].flaggedForRemoval || this.columns[ secondClickedPos[0] ][ secondClickedPos[1] ].flaggedForRemoval ){
@@ -158,10 +312,13 @@ function Grid(width, height, container){
 		
 		//nothing selected
 		else {
+			clickedElement.getContext('2d');
 
-		this.selectedJewelPos = [ columnIndex, rowIndex ];
-		//this.directions(columnIndex, rowIndex);
-		console.debug('selectedJewelPos: ', this.selectedJewelPos );
+
+			this.selectedJewelPos = [ columnIndex, rowIndex ];
+
+			console.debug('selectedJewelPos: ', this.selectedJewelPos );
+
 		}
 	}
 
